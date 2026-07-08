2b4dd03ac964d6df48c3465c4c44581cceddeb11
Shane Jaroch <chown_tee@proton.me>
2026-07-08 00:11:59 -0400

    init TestMSC4500StateHashMismatch

--- tests/msc4500/msc4500_test.go
+++ tests/msc4500/msc4500_test.go
@@ -1,16 +1,24 @@
 package msc4500
 
 import (
+	"context"
 	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/matrix-org/complement"
 	"github.com/matrix-org/complement/client"
+	"github.com/matrix-org/complement/federation"
 	"github.com/matrix-org/complement/helpers"
 	"github.com/matrix-org/complement/match"
 	"github.com/matrix-org/complement/must"
+	"github.com/matrix-org/gomatrixserverlib/fclient"
+	"github.com/tidwall/gjson"
 	"golang.org/x/crypto/blake2b"
 )
 
@@ -64,4 +72,71 @@ func TestMSC4500StateAccumulator(t *testing.T) {
 	expectedDigestHex := hex.EncodeToString(hash[:])
 
 	must.Equal(t, digestHex, expectedDigestHex, "Digest does not match BLAKE2b-256 of lattice")
+}
+
+func TestMSC4500StateHashMismatch(t *testing.T) {
+	deployment := complement.Deploy(t, 1)
+	defer deployment.Destroy(t)
+
+	alice := deployment.Register(t, "hs1", helpers.RegistrationOpts{})
+
+	// Create a remote homeserver
+	srv := federation.NewServer(t, deployment,
+		federation.HandleKeyRequests(),
+		federation.HandleMakeSendJoinRequests(),
+		federation.HandleTransactionRequests(nil, nil),
+	)
+	cancel := srv.Listen()
+	defer cancel()
+
+	// Alice creates a public room
+	roomID := alice.MustCreateRoom(t, map[string]interface{}{
+		"preset": "public_chat",
+	})
+
+	charlie := srv.UserID("charlie")
+	serverRoom := srv.MustJoinRoom(t, deployment, "hs1", roomID, charlie)
+
+	badEvent := srv.MustCreateEvent(t, serverRoom, federation.Event{
+		Sender: charlie,
+		Type:   "m.room.message",
+		Content: map[string]interface{}{
+			"msgtype": "m.text",
+			"body":    "Bad state hash event",
+		},
+	})
+
+	pdus := []json.RawMessage{badEvent.JSON()}
+	txnJSON := map[string]interface{}{
+		"origin":           srv.ServerName(),
+		"origin_server_ts": time.Now().UnixNano() / 1000000,
+		"pdus":             pdus,
+		"state_hashes": map[string]interface{}{
+			badEvent.EventID(): map[string]interface{}{
+				"algorithm": "lthash16",
+				"digest":    "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff",
+			},
+		},
+	}
+
+	txnID := fmt.Sprintf("txn-%d", time.Now().UnixNano())
+	reqURI := fmt.Sprintf("/_matrix/federation/v1/send/%s", txnID)
+
+	req := fclient.NewFederationRequest("PUT", srv.ServerName(), deployment.GetFullyQualifiedHomeserverName(t, "hs1"), reqURI)
+	err := req.SetContent(txnJSON)
+	must.NotError(t, "set content", err)
+
+	res, err := srv.DoFederationRequest(context.Background(), t, deployment, req)
+	must.NotError(t, "do federation request", err)
+
+	resBody, err := io.ReadAll(res.Body)
+	must.NotError(t, "read res body", err)
+
+	t.Logf("Response: %s", string(resBody))
+
+	// Verify the response contains state_hash_mismatch for the event
+	parsedRes := gjson.ParseBytes(resBody)
+	mismatchObj := parsedRes.Get(fmt.Sprintf("pdus.%s.state_hash_mismatch", badEvent.EventID()))
+	must.Equal(t, mismatchObj.Exists(), true, "state_hash_mismatch not found in response")
+	must.Equal(t, mismatchObj.Get("algorithm").Str, "lthash16", "mismatch algorithm wrong")
 }
