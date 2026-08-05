d181c93c41f3eed284acf7b765c4b0624cc1c344
Shane Jaroch <chown_tee@proton.me>
2026-08-04 21:25:59 -0400

    lint: remove now-unused `runtime` imports

--- tests/msc4500/msc4500_test.go
+++ tests/msc4500/msc4500_test.go
@@ -17,7 +17,6 @@ import (
 	"github.com/matrix-org/complement/helpers"
 	"github.com/matrix-org/complement/match"
 	"github.com/matrix-org/complement/must"
-	"github.com/matrix-org/complement/runtime"
 	"github.com/matrix-org/gomatrixserverlib/fclient"
 	"github.com/tidwall/gjson"
 	"golang.org/x/crypto/blake2b"
